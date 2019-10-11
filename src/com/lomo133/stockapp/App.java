3f73b7878cfcb2c4cb866f56ab690296c1568a31
joakiols <joakiols@kth.se>
2019-10-11 23:04:11 +0200

    small refactoring

--- src/com/lomo133/stockapp/App.java
+++ src/com/lomo133/stockapp/App.java
@@ -1,7 +1,5 @@
 package com.lomo133.stockapp;
 
-import java.io.FileNotFoundException;
-
 /**
  * @Author Joakim Olsson <lomo133>
  * Last edit: 2019-02-05
@@ -12,7 +10,7 @@ public class App {
     /**
      * Starting point for the application.
      */
-    public static void main(String[] args) throws FileNotFoundException {
+    public static void main(String[] args) {
         new Gui();
     }
 }
